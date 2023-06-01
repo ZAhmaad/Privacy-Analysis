4805487ba444686142f404b9e04a4f3123ce7111
Zubairahmad09 <78538043+Zubairahmad09@users.noreply.github.com>
2023-06-01 16:33:07 +0200

    Add files via upload

---
+++ analysis/src/index.ts
@@ -0,0 +1,47 @@
+import * as mockttp from "mockttp";
+import startProxyServer from "./lib/proxy-server";
+import { startAnalysis } from "./lib/start-analysis";
+import BrowserManager from "./lib/browser-manager";
+import {
+  getArgFile,
+  getArgLength,
+  getArgOffset,
+  readSiteListFromTrancoListFile,
+} from "./lib/input";
+
+async function main() {
+  const args = require("minimist")(process.argv.slice(2));
+
+  const siteList = await readSiteListFromTrancoListFile(
+    getArgFile(args),
+    getArgOffset(args),
+    getArgLength(args)
+  );
+  console.log(siteList);
+  console.log(`${siteList.length} sites`);
+
+  const certOptions = await mockttp.generateCACertificate();
+  const caFingerprint = mockttp.generateSPKIFingerprint(certOptions.cert);
+  const server = await startProxyServer(certOptions);
+
+  const browserManager = new BrowserManager({
+    proxyPort: server.port,
+    proxyCaFingerprint: caFingerprint,
+    headless: true,
+  });
+  await browserManager.launchAll();
+
+  await startAnalysis(browserManager, siteList);
+
+  await browserManager.closeAll();
+
+  await server.stop();
+
+  console.log("THE END");
+}
+
+process.setUncaughtExceptionCaptureCallback((err) => {
+  console.error(err);
+});
+
+main();
